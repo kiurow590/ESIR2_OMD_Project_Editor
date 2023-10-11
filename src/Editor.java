357576766524e7a11702a70d09ed9eff13677749
kiurow590 <kiurow.geek@gmail.com>
2023-10-11 22:02:04 +0200

    Changes by Thibault

--- src/Editor.java
+++ src/Editor.java
@@ -36,12 +36,102 @@ public class Editor {
         final JScrollPane upperScroll = new JScrollPane(panel_2, JScrollPane.VERTICAL_SCROLLBAR_NEVER,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
+        Editor editor = this;
+        JButton btnCopy = new JButton("Copy");
+        btnCopy.setBounds(50, 210, 180, 30);
+            btnCopy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform actions specific to Button 1
+                // You can identify it based on its label: "Button 1"
+                // For example:
+                //setCopyBuffer(textArea.getSelectedText());
+                Command copy = CopyCommand.getInstance(editor);
+                copy.execute();
+                System.out.println("BOUTON COPY CLICK");
+            }
+        });
 
-        panel_2.add((new JButton("Copy")));
+
+        panel_2.add(btnCopy);
+
+        l1 = new JLabel();
+        l1.setBounds(45, 175, 100, 30);
+
+        panel_2.add(l1);
 
 
-        panel_2.add(new JButton("Paste"));
-        panel_2.add(new JButton("Cut"));
+        JButton btnPaste = new JButton("Paste");
+        btnPaste.setBounds(50, 210, 180, 30);
+        btnPaste.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform actions specific to Button 1
+                // You can identify it based on its label: "Button 1"
+                // For example:
+                //l1.setText(getCopyBuffer());
+                //textArea.append(getCopyBuffer());
+                Command paste = PasteCommand.getInstance(editor);
+                paste.execute();
+                System.out.println("BOUTON Paste CLICK");
+            }
+        });
+        panel_2.add(btnPaste);
+
+
+        JButton btnCut = new JButton("Cut");
+        btnCut.setBounds(50, 210, 180, 30);
+        btnCut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform actions specific to Button 1
+                // You can identify it based on its label: "Button 1"
+                // For example:
+                //l1.setText(textArea.getSelectedText());
+                //String test textArea.getText();
+                Command paste = CutCommand.getInstance(editor);
+                paste.execute();
+                System.out.println("BOUTON Cut CLICK");
+            }
+        });
+        panel_2.add(btnCut);
+
+        final JButton btnSelectRight = new JButton("->");
+        btnSelectRight.setBounds(50, 210, 180, 30);
+        //JTextField selection = new JTextField();
+
+
+        btnSelectRight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform actions specific to Button 1
+                // You can identify it based on its label: "Button 1"
+                // For example:
+                //l1.setText(textArea.getSelectedText());
+                //String test textArea.getText();
+                //textArea.selectAll();
+                /*Command selectRight = new SelectRightCommand(editor);
+                selectRight.execute();*/
+
+                //textArea.selectAll();
+
+                // Transfer focus to JTextArea to show the selected
+                // text.
+                //btnSelectRight.transferFocusBackward();
+                //int currentRows = textArea.getRows();
+                //int currentColumns = textArea.getColumns();
+                //textArea.setRows(currentRows + 1);
+                //textArea.setColumns(currentColumns + 10);
+                textArea.selectAll();
+
+                // Transfer focus to JTextArea to show the selected
+                // text.
+                btnSelectRight.transferFocusBackward();
+                System.out.println("BOUTON SelectRight CLICK");
+            }
+        });
+        panel_2.add(btnSelectRight);
+
         GridLayout griddy = new GridLayout(2, 0);
         window.setLayout(griddy);
         window.add(BorderLayout.SOUTH, upperScroll);
